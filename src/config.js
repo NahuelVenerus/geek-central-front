5d9bf9512762c888780070ac8912c8845d7050e0
Nahuel Venerus <nahuelvenerus@gmail.com>
2023-07-11 16:38:21 -0300

    services first commit

---
+++ src/config.js
@@ -0,0 +1,3 @@
+require("dotenv").config();
+
+export const BASE_ROUTE = process.env.REACT_APP_BASE_ROUTE;
---
+++ src/services/admin/getAllUsers.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { BASE_ROUTE } from "../../config";
+
+export const getAllUsers = async (nickname) => {
+  try {
+    const users = await axios.get(`${BASE_ROUTE}/users/${nickname}`);
+    return users;
+  } catch (error) {
+    console.log("getAllUsers service error", error);
+  }
+};
