a70f2c54084bcdc1a5ac90cdd6689c5707dfb5c5
Nahuel Venerus <nahuelvenerus@gmail.com>
2023-07-12 19:10:23 -0300

    second searcher version

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { Provider } from "react-redux";
 import SignUp from "./components/SignUp";
 import LogIn from "./components/LogIn";
 import Home from "./components/Home";
-import store from "./state/store";
 
 function App() {
   return (
-    <Provider store={store}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signUp" element={<SignUp />} />
-        <Route path="/login" element={<LogIn />} />
-      </Routes>
-    </Provider>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signUp" element={<SignUp />} />
+      <Route path="/login" element={<LogIn />} />
+    </Routes>
   );
 }
 
