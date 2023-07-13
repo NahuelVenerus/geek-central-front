3e07d77e25a56174c0fed94fe999d0473321335f
Nahuel Venerus <nahuelvenerus@gmail.com>
2023-07-13 17:30:26 -0300

    profile first version

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import SignUp from "./components/SignUp";
 import LogIn from "./components/LogIn";
 import Home from "./components/Home";
+import Profile from "./components/Profile";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/signUp" element={<SignUp />} />
       <Route path="/login" element={<LogIn />} />
+      <Route path="/profile/" element={<Profile />} />
     </Routes>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,134 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/profileStyles.css";
+// import { useSelector } from "react-redux";
+import {
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+  MDBCard,
+  MDBCardText,
+  MDBCardBody,
+  MDBCardImage,
+  MDBProgress,
+  MDBProgressBar,
+  MDBIcon,
+  MDBListGroup,
+  MDBListGroupItem,
+  MDBBtn,
+} from "mdb-react-ui-kit";
+
+const user = {
+  id: 1,
+  name: "Nahuel",
+  nickname: "Ashneroh",
+  lastname: "Venerus",
+  address: "Nahue's House",
+  img: "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp",
+  phone: 1136441633,
+  zip_code: 1228,
+  city: "Ciudad Autónoma de Buenos Aires",
+  email: "nahuelvenerus@gmail.com",
+  password: "Nahue123",
+  is_admin: false,
+  is_deleted: false,
+};
+
+export default function Profile() {
+  const {
+    name,
+    nickname,
+    lastname,
+    address,
+    img,
+    phone,
+    zip_code,
+    email,
+    city,
+  } = user;
+  // const user = useSelector((state) => state.user);
+  return (
+    <MDBContainer className="profileContainer py-5">
+      <MDBRow>
+        <MDBCol lg="4">
+          <MDBCard className="mb-4">
+            <MDBCardBody className="text-center">
+              <MDBCardImage
+                src={img}
+                alt="avatar"
+                className="profilePicture rounded-circle"
+                fluid
+              />
+              <p className="nickname text-muted mb-4">{nickname}</p>
+              <div className="d-flex justify-content-center mb-2">
+                <MDBBtn>Editar Perfil</MDBBtn>
+                <MDBBtn className="ms-1 btn btn-danger">Borrar Perfil</MDBBtn>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol lg="8">
+          <MDBCard className="text-center mb-4">
+            <MDBCardBody>
+              <MDBRow>
+                <MDBCol sm="3">
+                  <MDBCardText>Nombre Completo</MDBCardText>
+                </MDBCol>
+                <MDBCol sm="9">
+                  <MDBCardText className="text-muted">
+                    {name} {lastname}
+                  </MDBCardText>
+                </MDBCol>
+              </MDBRow>
+              <hr />
+              <MDBRow>
+                <MDBCol sm="3">
+                  <MDBCardText>Email</MDBCardText>
+                </MDBCol>
+                <MDBCol sm="9">
+                  <MDBCardText className="text-muted">{email}</MDBCardText>
+                </MDBCol>
+              </MDBRow>
+              <hr />
+              <MDBRow>
+                <MDBCol sm="3">
+                  <MDBCardText>Numero de Teléfono</MDBCardText>
+                </MDBCol>
+                <MDBCol sm="9">
+                  <MDBCardText className="text-muted">{phone}</MDBCardText>
+                </MDBCol>
+              </MDBRow>
+              <hr />
+              <MDBRow>
+                <MDBCol sm="3">
+                  <MDBCardText>Dirección</MDBCardText>
+                </MDBCol>
+                <MDBCol sm="9">
+                  <MDBCardText className="text-muted">{address}</MDBCardText>
+                </MDBCol>
+              </MDBRow>
+              <hr />
+              <MDBRow>
+                <MDBCol sm="3">
+                  <MDBCardText>Código Postal</MDBCardText>
+                </MDBCol>
+                <MDBCol sm="9">
+                  <MDBCardText className="text-muted">{zip_code}</MDBCardText>
+                </MDBCol>
+              </MDBRow>
+              <hr />
+              <MDBRow>
+                <MDBCol sm="3">
+                  <MDBCardText>Localidad</MDBCardText>
+                </MDBCol>
+                <MDBCol sm="9">
+                  <MDBCardText className="text-muted">{city}</MDBCardText>
+                </MDBCol>
+              </MDBRow>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+}
